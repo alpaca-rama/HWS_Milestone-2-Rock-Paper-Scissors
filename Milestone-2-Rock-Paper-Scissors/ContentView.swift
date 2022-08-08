0c5dbcae1b8f5b4ee933504094852a22ad917f36
Luca <luca@weve-got-it.com>
2022-08-08 21:49:08 +0200

    Initial Commit

---
+++ Milestone-2-Rock-Paper-Scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Milestone-2-Rock-Paper-Scissors
+//
+//  Created by Luca Capriati on 2022/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
