e08a07a7c91a1d762b3b3ab0f071822b329ffd59
Luca <luca@weve-got-it.com>
2022-08-09 22:14:05 +0200

    Challenge: Created the State variable to generate the possible moves, the winning boolean and player score. The arrays for possible moves, winning moves and losing moves. The basic UI to display the componants. The functions to cater for the decisions to be made. The player score to be displayed to ensure the logic is accurate. A next function to ensure the player can player another round.

--- Milestone-2-Rock-Paper-Scissors/ContentView.swift
+++ Milestone-2-Rock-Paper-Scissors/ContentView.swift
@@ -8,9 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var appChoice = Int.random(in: 0...2)
+    @State private var appWinOrLose = Bool.random()
+    @State private var playerScore = 0
+    
+    let possibleMoves = ["Rock", "Paper", "Scissors"]
+    let winningMoves = ["Paper", "Scissors", "Rock"]
+    let losingMoves = ["Scissors", "Rock", "Paper"]
+    
+    var appWinOrLoseText: String {
+        var condition: String
+        
+        if appWinOrLose == true {
+                condition = "Win"
+        } else {
+            condition = "Lose"
+        }
+        
+        return condition
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Player Score: \(playerScore)")
+            Spacer()
+            if appWinOrLoseText == "Win" {
+                VStack {
+                    Text("BEAT")
+                        .foregroundColor(.green)
+                        .font(.title.bold())
+                    Text(possibleMoves[appChoice])
+                        .font(.largeTitle.bold())
+                }
+            } else if appWinOrLoseText == "Lose" {
+                VStack {
+                    Text("LOSE TO")
+                        .foregroundColor(.red)
+                        .font(.title.bold())
+                    Text(possibleMoves[appChoice])
+                        .font(.largeTitle.bold())
+                }
+            }
+            Spacer()
+            HStack {
+                ForEach(0..<3) {number in
+                    Button {
+                        playerChoice(number)
+                    } label: {
+                        Text(possibleMoves[number])
+                            .font(.largeTitle)
+                    }
+                }
+            }
+        }
+    }
+    
+    func playerChoice(_ number: Int) {
+        if appWinOrLoseText == "Win" {
+            if possibleMoves[number] == winningMoves[appChoice] {
+                correctChoice()
+            } else if possibleMoves[number] == possibleMoves[appChoice] {
+                draw()
+            } else {
+                incorrectScore()
+            }
+        } else if appWinOrLoseText == "Lose" {
+            if possibleMoves[number] == losingMoves[appChoice] {
+                correctChoice()
+            } else if possibleMoves[number] == possibleMoves[appChoice] {
+                draw()
+            } else {
+                incorrectScore()
+            }
+        }
+        
+        next()
+    }
+    
+    func correctChoice() {
+        playerScore += 1
+        print("CORRECT")
+    }
+    
+    func incorrectScore() {
+        playerScore -= 1
+        print("INCORRECT")
+        
+        if playerScore < 0 { playerScore = 0 }
+    }
+    
+    // future functionality, if wanted
+    func draw() {
+        playerScore += 0
+        print("DRAW")
+    }
+    
+    func next() {
+        appChoice = Int.random(in: 0...2)
+        appWinOrLose = Bool.random()
+    }
+    
+    func reset() {
+        playerScore = 0
     }
 }
 
