b4a616092ceef050615fdd48e33e0c63408f8ef4
Luca <luca@weve-got-it.com>
2022-08-22 14:21:56 +0200

    Revised: Going over all the projects to fix everything and re do challenges.

--- Milestone-2-Rock-Paper-Scissors/ContentView.swift
+++ Milestone-2-Rock-Paper-Scissors/ContentView.swift
@@ -8,139 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var appChoice = Int.random(in: 0...2)
-    @State private var appWinOrLose = Bool.random()
-    @State private var playerScore = 0
-    @State private var question = 1
-    @State private var questionsAlert = false
+    @State private var computerChoice = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    @State private var quesetionCount = 1
+    @State private var showingResults = false
     
-    let numberOfQuestions = 10
-    
-    let possibleMoves = ["Rock", "Paper", "Scissors"]
-    let winningMoves = ["Paper", "Scissors", "Rock"]
-    let losingMoves = ["Scissors", "Rock", "Paper"]
-    let emojiMoves = ["👊🏼", "✋🏼", "✌🏼"]
-    
-    var appWinOrLoseText: String {
-        var condition: String
-        
-        if appWinOrLose == true {
-                condition = "Win"
-        } else {
-            condition = "Lose"
-        }
-        
-        return condition
-    }
+    let moves = ["👊🏼", "✋🏼", "✌🏼"]
     
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.red, .orange]), startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea(.all)
-            VStack {
-                Text("Question \(question) of \(numberOfQuestions)")
-                    .font(.system(size: 25))
-//                Text("Player Score: \(playerScore)")
-                Spacer()
-                if appWinOrLoseText == "Win" {
-                    VStack {
-                        Text("BEAT")
-                            .foregroundColor(.green)
-                            .font(.title.bold())
-                        Text(emojiMoves[appChoice])
-                            .font(.system(size: 80))
-                    }
-                } else if appWinOrLoseText == "Lose" {
-                    VStack {
-                        Text("LOSE TO")
-                            .foregroundColor(.red)
-                            .font(.title2.bold())
-                        Text(emojiMoves[appChoice])
-                            .font(.system(size: 80))
-                    }
-                }
-                Spacer()
-                HStack {
-                    ForEach(0..<3) {number in
-                        Button {
-                            playerChoice(number)
-                        } label: {
-                            Text(emojiMoves[number])
-                                .font(.system(size: 80))
-                                .foregroundColor(appWinOrLoseText == "Win" ? .green : .red)
-                        }
-                    }
-                }
-                
+        VStack {
+            Spacer()
+            Text("Computer has played...")
+                .font(.headline)
+            Text(moves[computerChoice])
+                .font(.system(size: 200))
+            
+            if shouldWin {
+                Text("Which one wins?")
+                    .foregroundColor(.green)
+                    .font(.title)
+            } else {
+                Text("Which one loses?")
+                    .foregroundColor(.red)
+                    .font(.title)
             }
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 20)
-            .background(.regularMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .padding()
+            
+            HStack {
+                ForEach(0..<3) { number in
+                    Button(moves[number]) {
+                        play(choice: number)
+                    }
+                    .font(.system(size: 80))
+                }
+            }
+            
+            Spacer()
+            
+            Text("Score: \(score)")
+                .font(.subheadline)
+            
+            Spacer()
         }
-        .alert("You scored: \(playerScore) points", isPresented: $questionsAlert) {
-            Button("no") { }
-            Button("YES", action: reset)
+        .alert("Game over", isPresented: $showingResults) {
+            Button("Play Again", action: reset)
         } message: {
-            Text("Do you want to play again?")
+            Text("Your score was \(score).")
         }
     }
     
-    func playerChoice(_ number: Int) {
-        if appWinOrLoseText == "Win" {
-            if possibleMoves[number] == winningMoves[appChoice] {
-                correctChoice()
-            } else if possibleMoves[number] == possibleMoves[appChoice] {
-                draw()
-            } else {
-                incorrectScore()
-            }
-        } else if appWinOrLoseText == "Lose" {
-            if possibleMoves[number] == losingMoves[appChoice] {
-                correctChoice()
-            } else if possibleMoves[number] == possibleMoves[appChoice] {
-                draw()
-            } else {
-                incorrectScore()
-            }
-        }
+    func play(choice: Int) {
+        let winningMoves = [1, 2, 0]
+        let didWin: Bool
         
-        nextQuestion()
-    }
-    
-    func correctChoice() {
-        playerScore += 1
-        print("CORRECT")
-    }
-    
-    func incorrectScore() {
-        playerScore -= 1
-        print("INCORRECT")
-        
-        if playerScore < 0 { playerScore = 0 }
-    }
-    
-    // future functionality, if wanted
-    func draw() {
-        playerScore += 0
-        print("DRAW")
-    }
-    
-    func nextQuestion() {
-        if question < numberOfQuestions {
-            appChoice = Int.random(in: 0...2)
-            appWinOrLose = Bool.random()
-            question += 1
+        if shouldWin {
+            didWin = choice == winningMoves[computerChoice]
         } else {
-            questionsAlert = true
+            didWin = winningMoves[choice] == computerChoice
+        }
+        
+        if didWin {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        if quesetionCount == 3 {
+            showingResults = true
+        } else {
+            computerChoice = Int.random(in: 0..<3)
+            shouldWin.toggle()
+            quesetionCount += 1
         }
     }
     
     func reset() {
-        playerScore = 0
-        question = 0
-        nextQuestion()
+        computerChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+        quesetionCount = 0
+        score = 0
     }
 }
 
